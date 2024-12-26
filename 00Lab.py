9ba5c1140b684c4034d6a430f289f184bdf8a6e3
Ian Marsh <60201744+IanMarsh1@users.noreply.github.com>
2024-12-26 10:58:58 -0500

    First commit :)

---
+++ 00Lab.py
@@ -0,0 +1,15 @@
+print('\n\nOutput:\n-----------------')
+
+#! Python! YIPPEEE 🎉
+
+#TODO Print Hello World!
+
+#TODO What is a variable?
+
+#TODO What is a data type
+
+#TODO Casting
+
+#TODO Operators
+
+print('\n\n')
---
+++ 00LabA.py
@@ -0,0 +1,45 @@
+print('\n\nOutput:\n-----------------')
+
+#! Python! YIPPEEE 🎉
+
+#TODO print Hello World!
+print("Hello World!")
+print()
+
+#TODO what is a variable?
+age = 23 # int
+temp = 72.3 # float
+
+name = "Ian" # String
+last = 'Marsh' # Still a string
+
+flag = False # bool 
+flag = True # mutable - the ability of an object to be changed after it is created
+
+listOfNumbers = [1,2,3,4]
+
+listOfNumbers.append(5)
+
+print(listOfNumbers.count(4))
+print(listOfNumbers.index(4))
+
+#TODO what is a data type
+print(type(listOfNumbers))
+
+#TODO casting
+x = 0b11111111
+x = float(x)
+
+print(type(x))
+print(x)
+
+# + - * / % ** //
+# += -= (no ++ 🥲)
+# >= <= == != < >
+# and or not
+x = 8
+y = 3
+
+print(not(2 > 5) and (3 >= 3))
+
+print('\n\n')
