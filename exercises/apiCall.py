f39425e431d65d62754ccc212e41f335e58c76b9
Ian Marsh <60201744+IanMarsh1@users.noreply.github.com>
2024-12-27 07:46:28 +0000

    api

---
+++ exercises/apiCall.py
@@ -0,0 +1,2 @@
+# https://swagger-ui.meteomatics.com/
+# https://api.weather.gov/openapi.yaml
---
+++ labs/02LabA.py
@@ -0,0 +1,27 @@
+print('\n\nOutput:\n-----------------')
+
+#! Python! YIPPEEE 🎉
+
+#TODO Make a Function
+def myFunction():
+  print("Hello from a function")
+  
+myFunction()
+
+#TODO pass a var to a Function
+def isEven(number):
+    if (number % 2) == 0:
+        return True
+    return False
+
+print(isEven(2))
+
+#TODO import a function
+import mathFunctions
+
+print(mathFunctions.retunrPow(2, 4))
+
+PI = mathFunctions.returnPI()
+print(PI)
+
+print('\n\n')
