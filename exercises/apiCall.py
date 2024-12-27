bab7b02a6dd53e867a5972c5e037aba2252031c6
Ian Marsh <60201744+IanMarsh1@users.noreply.github.com>
2024-12-27 11:55:30 +0000

    added api X

--- exercises/apiCall.py
+++ exercises/apiCall.py
@@ -1,2 +1,14 @@
-# https://swagger-ui.meteomatics.com/
-# https://api.weather.gov/openapi.yaml
+# apiCall
+
+#? The goal of this x is to make a simple api call and print some of the data from the response to the consol
+#? The server I picked to do an api call to is weather.gov
+#? Expected Output: The temperature in [?] is [?]
+#? Use the following links and your friend google to get started
+
+#* Look at an example api documentation in swagger https://swagger-ui.meteomatics.com/
+#* In the search bar in swagger, put in the url https://api.weather.gov/openapi.yaml
+
+#* https://www.geeksforgeeks.org/how-to-make-api-calls-using-python/#
+
+# might also need to know how to work with dict
+#* https://www.w3schools.com/python/python_ref_dictionary.asp
---
+++ exercises/apiCallA.py
@@ -0,0 +1,47 @@
+# apiCallA
+#* Look at an example api documentation in swagger https://swagger-ui.meteomatics.com/
+#* In the search bar, put in the url https://api.weather.gov/openapi.yaml
+#* https://www.geeksforgeeks.org/how-to-make-api-calls-using-python/#
+#* https://www.w3schools.com/python/python_ref_dictionary.asp
+
+import requests
+
+def get_posts(uri):
+    # https://www.w3schools.com/python/python_try_except.asp
+    try:
+        # go out and ask the server for the info at this uri
+        response = requests.get(uri)
+
+        # look at HTTP status codes https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+        if response.status_code == 200:
+            posts = response.json()
+            return posts
+        else:
+            print('Error:', response.status_code)
+            return None
+        
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+        return None
+
+def main():
+    # find a station id using the /stations in swagger API
+    stationID = "0084W"
+    
+    # each URI is for each api call
+    tempURI = f'https://api.weather.gov/stations/{stationID}/observations/latest'
+    locURI = f'https://api.weather.gov/stations/{stationID}'
+    
+    # make the api call
+    temp = get_posts(tempURI)
+    loc = get_posts(locURI)
+    
+    # convert data type to sring so it can be printed and get the values from the dictionary I want
+    # note: this is not the best way to do this
+    temp = str(temp['properties']['temperature']['value'])
+    loc = str(loc['properties']['name'])
+    
+    print("The temperature in " + loc + " is " + temp + "C")
+        
+if __name__ == '__main__':
+    main()
