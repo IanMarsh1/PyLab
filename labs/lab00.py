4a204c7f90ce5ad0a8a5e3a8d365ce886fba36bb
Ian Marsh <60201744+IanMarsh1@users.noreply.github.com>
2024-12-27 12:39:51 -0500

    update

--- labs/lab00.py
+++ labs/lab00.py
@@ -2,119 +2,14 @@ print('\n\nOutput:\n-----------------')
 
 #! Python! YIPPEEE 🎉
 
-
-
-
-
-
 #TODO Print Hello World!
-#print("Hello World!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #TODO What is a variable?
-age = 23 # int
-temp = 72.3 # float
-
-fName = "Ian" # string
-lName = 'Marsh' # string 
-
-flag = True # bool
-flag = False # bool
-
-listOfNums = [1, 2, 3, 4] # list
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #TODO What is a data type
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #TODO Casting
-y = "42"
-
-y = int(y)
-y = float(y)
-y = str(y)
-
-print(y)
-print(type(y))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #TODO Operators
-
-
-
-
 
 print('\n\n')
